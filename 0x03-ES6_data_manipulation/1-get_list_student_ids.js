e8447115a1bbcb23754ce21a8a49fa3144d46b36
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-10-07 18:14:34 +0100

    Return ids of objects not indexes

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,11 @@
+/* returns an array of ids from a list of object. */
+
 export default function getListStudentIds(objArray) {
   if (!Array.isArray(objArray)) {
     return [];
   }
 
-  const arrayId = objArray.map((obj) => objArray.indexOf(obj));
+  const arrayId = objArray.map((obj) => obj.id);
 
   return arrayId;
 }
