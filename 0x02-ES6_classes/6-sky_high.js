ec06734190007d4c217fbf8440bc15581cfee41e
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-20 14:12:29 +0100

    Extend a class from another

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-underscore-dangle */
+
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
