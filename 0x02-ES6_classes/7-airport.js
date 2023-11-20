1837045c9b20a29d788ca946586377b30b99844c
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-11-20 14:47:54 +0100

    Override default string decription of a class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-underscore-dangle */
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
