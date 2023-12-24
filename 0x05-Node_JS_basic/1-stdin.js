e7a1fdebb60a0d6e621f3688c427faa721b411b8
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-24 22:46:22 +0100

    Create 1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdin.setEncoding("utf-8");
+
+process.stdout.write("Welcome to Holberton school. What is your name?\n");
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+
+  process.stdout.write(`Your name is: ${name}\n`);
+
+  process.stdout.write("This important software is now closing\n");
+
+  process.exit();
+})
