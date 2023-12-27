aba43eac9cc21cb85f2f7440ddc657ff890bdb81
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-27 22:41:08 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
-process.stdin.setEncoding("utf-8");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-
-  process.stdout.write(`Your name is: ${name}\n`);
-
-  process.stdout.write("This important software is now closing\n");
-
-  process.exit();
-})
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
