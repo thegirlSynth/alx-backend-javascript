64f2e32aef735359daf47df9dbfd2e1e41b0fbda
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-28 02:06:15 +0100

    Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const students = data.split('\n').filter(Boolean);
+    const fields = {};
+
+    let totalStudents = 0;
+
+    students.slice(1).forEach((student) => {
+      // eslint-disable-next-line no-unused-vars
+      const [firstName, lastname, age, field] = student.split(',');
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+      totalStudents += 1;
+    });
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
