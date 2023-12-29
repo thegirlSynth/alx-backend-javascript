2e58ba0816ebf88cec2030b6f7c164aafc6d0455
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-29 03:48:38 +0100

    Creating and using Spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledOnce).to.be.true;
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+});
