0b1b376825bd51242fdcd1d37047b378b53f9017
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-29 02:47:51 +0100

    Add more test suites

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,22 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', function () {
-  it('should return the sum of rounded numbers', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+describe('calculateNumber', () => {
+  it('rounding of a', () => {
+    assert.equal(calculateNumber(15.78, 2), 18);
+    assert.equal(calculateNumber(1.2, 0), 1);
+    assert.equal(calculateNumber(3.5, 2), 6);
   });
-});
+
+  it('rounding of b', () => {
+    assert.equal(calculateNumber(2, 15.78), 18);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(2, 3.5), 6);
+  });
+
+  it('suming of a and b', () => {
+    assert.equal(calculateNumber(15.78, 2.5), 19);
+    assert.equal(calculateNumber(1.2, 0.2), 1);
+    assert.equal(calculateNumber(3.5, 2.7), 7);
+  });
+})
