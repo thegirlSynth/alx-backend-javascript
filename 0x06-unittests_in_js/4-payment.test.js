b54658f32c6802f399063397ef3dc3b342c43024
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-29 03:57:42 +0100

    Creating and using Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should stub Utils.calculateNumber and log correct message', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+  });
+});
