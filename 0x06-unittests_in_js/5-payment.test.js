0211a01c7a76eceb1a3fdc0150e37d002f5b8022
Cynthia Uche - [Linux] <cynthianuchepro@gmail.com>
2023-12-29 04:00:50 +0100

    Creating and using Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should log correct message for 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log correct message for 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
